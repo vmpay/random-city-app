069a0316c76b6204b8fe901bce57932b7511e7f6
Andrew <vmpay.eu@gmail.com>
2021-02-13 18:33:32 +0100

    Initial commit

---
+++ app/src/main/java/eu/vmpay/random/city/MainActivity.kt
@@ -0,0 +1,11 @@
+package eu.vmpay.random.city
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
